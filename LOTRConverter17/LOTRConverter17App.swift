4d6ad9d3c8f20b6b8ab93de67dcbf5394030a2ca
Mikołaj Kozłowski <m.kozlowski1221@gmail.com>
2024-06-13 18:00:18 +0200

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Mikołaj on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOTRConverter17UITests/LOTRConverter17UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LOTRConverter17UITestsLaunchTests.swift
+//  LOTRConverter17UITests
+//
+//  Created by Mikołaj on 13/06/2024.
+//
+
+import XCTest
+
+final class LOTRConverter17UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
